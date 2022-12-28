d0d552fe0a64dec96cf13467aed55ed8f0319f18
g-Barsani <gabrielbarsani@gmail.com>
2022-12-28 19:13:42 -0300

    Create method to clear all fields

--- MusicCollection/Utilities.cs
+++ MusicCollection/Utilities.cs
@@ -5,5 +5,28 @@ namespace MusicCollection
 {
     public class Utilities
     {
+        public static void ResetAllControls(Control form)
+        {
+            /*
+             The Control class implements very basic functionality required by classes 
+            that display information to the user. It handles user input through the keyboard 
+            and pointing devices. 
+            */
+            foreach (Control control in form.Controls)
+            {
+                if (control is TextBox)
+                {
+                    // Explicit casting
+                    var textBox = (TextBox)control;
+                    textBox.Text = null;
+                }
+                if (control is ComboBox)
+                {
+                    var comboBox = (ComboBox)control;
+                    comboBox.SelectedIndex = -1;
+                }
+            }
+        }
+
     }
 }
